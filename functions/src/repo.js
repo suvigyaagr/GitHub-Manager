8c69d1fbb262230c1973ec28db79fb6d1ba3b8cc
Suvigya Agrawal <suvigya1st@gmail.com>
2019-06-23 00:34:01 +0530

    Added Main Suggestions after getting the list of repositeries

--- functions/src/repo.js
+++ functions/src/repo.js
@@ -58,6 +58,7 @@ module.exports = {
             title: `List of repositeries`,
             items: listData,
           });
+          ar.askUser(conv, aogResponseType.SUGGESTIONS, rsp.MAIN_SUGGESTIONS);
           return;
         },
         (error) => {
